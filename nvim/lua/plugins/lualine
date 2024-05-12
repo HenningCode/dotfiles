5f9bce387714b605a3306caadd8a88ba26081a8b
HenningCode <henningwilmer@yahoo.de>
2024-05-12 07:22:34 +0200

    add nvim

---
+++ nvim/lua/plugins/lualine
@@ -0,0 +1,1 @@
+../../../../dotfiles/.config/nvim/lua/plugins/lualine.lua
---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,22 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
